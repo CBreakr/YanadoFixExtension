42027a21dc594897ddc91510aa618edcf2b32266
CBreakr <rob.notwicz@gmail.com>
2019-02-10 23:54:59 -0500

    initial clicking works

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("script in page");
+RunSetup();
+
+function RunSetup(){
+  document.querySelector('body').addEventListener('click', function (event) {
+    if (event.target.classList.contains('yn-group-kanban-view-name')
+	|| event.target.classList.contains('yn-group-list-view-name')
+	|| event.target.classList.contains('yn-group-card-view-name')
+    ) {
+        // Do something
+	alert("target element");
+    }
+  });
+}
