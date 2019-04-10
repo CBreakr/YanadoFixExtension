9efe553006f96f850860eecea1210e6c2279dbea
CBreakr <rob.notwicz@gmail.com>
2019-04-10 16:31:53 -0400

    removed etraneous testing

--- secondscript.js
+++ secondscript.js
@@ -36,19 +36,3 @@ function Launch(){
   console.log("LAUNCHED LAUNCHED LAUNCHED");
   //AttemptAPICall();
 }
-
-function AttemptAPICall(){
-  const xhr = new XMLHttpRequest();
-  xhr.timeout = 10000;
-  xhr.onload = function() {
-    if (xhr.status == 200) {
-      console.log(`XHR RESPONSE: ${xhr.response}`);
-    }
-    else{
-      console.log(`XHR STATUS for GET TEST: ${xhr.status}`);
-    }
-  };
-  xhr.open("GET", "https://api.yanado.com/public-api/lists", true);
-  xhr.setRequestHeader('X-API-Key', 'd2c34c57-3521-47a0-983e-77916347d2c5');
-  xhr.send();
-}
